2cf24469f1097d7afdfbbeffb0acc2ff75a2a38f
Evan Timmons <timmonsevan@gmail.com>
2025-10-14 15:19:16 -0400

    fixed imports

--- demo/src/main/java/com/MEW/demo/model/User.java
+++ demo/src/main/java/com/MEW/demo/model/User.java
@@ -1,6 +1,7 @@
+package com.MEW.demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "User")
