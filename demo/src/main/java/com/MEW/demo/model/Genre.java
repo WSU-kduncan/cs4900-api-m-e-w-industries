1f6558f156ce60e08472b9a83bb6088eb19a35e9
Evan Timmons <timmonsevan@gmail.com>
2025-10-17 20:17:36 -0400

    updated all POJOs to include @NoArgsConstructor and @AllArgsConstructor

--- demo/src/main/java/com/MEW/demo/model/Genre.java
+++ demo/src/main/java/com/MEW/demo/model/Genre.java
@@ -1,17 +1,20 @@
 package com.MEW.demo.model;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "Genre")
 public class Genre {
     
